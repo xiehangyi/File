59e598e7bbee0b3d102942720d79578f1fd26a1e
xhy <www.xiehangyi@qq.com>
2016-06-10 12:59:08 +0800

    增加了按文件大小排序，名称排序功能；增加了新建文件，重命名文件功能；

--- app/src/main/java/com/example/xhy/file/FileAdapter.java
+++ app/src/main/java/com/example/xhy/file/FileAdapter.java
@@ -1,12 +1,17 @@
 package com.example.xhy.file;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
@@ -15,7 +20,10 @@ import android.widget.Toast;
 import android.content.DialogInterface;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * FileAdapter 显示文件列表
@@ -26,6 +34,7 @@ public class FileAdapter extends BaseAdapter {
     private ArrayList<File> data;
 
     private LayoutInflater layoutInflater;
+    private static HashMap<String ,String> map;
 
     /**
      * 创建文件适配器(数据和视图间的桥)
@@ -37,6 +46,7 @@ public class FileAdapter extends BaseAdapter {
 
         this.context = context;
         this.data = data;
+        map = new HashMap<>();
 
         layoutInflater = LayoutInflater.from(context);
     }
@@ -91,7 +101,7 @@ public class FileAdapter extends BaseAdapter {
             convertView = layoutInflater.inflate(R.layout.file_item, parent, false);
 
             //  获得【视图项】的结构
-            holder = new ViewHolder(convertView);
+            holder = new ViewHolder(convertView, position);
 
             //  为视图绑定一个数据（结构）
             convertView.setTag(holder);
@@ -100,7 +110,11 @@ public class FileAdapter extends BaseAdapter {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        holder.bindData(data.get(position));
+        try {
+            holder.bindData(data.get(position));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
         return convertView;
@@ -119,18 +133,27 @@ public class FileAdapter extends BaseAdapter {
         private TextView fileName;
         private TextView fileInfo;
         private ImageButton more;
+        private TextView filesize;
+
+        private int position;
+        File file;
 
         PopMenuListener listener;
 
-        public ViewHolder(View v) {
+
+        public ViewHolder(View v, int position) {
+
 
             icon = (ImageView) v.findViewById(R.id.imageView);
             fileName = (TextView) v.findViewById(R.id.textView_name);
             fileInfo = (TextView) v.findViewById(R.id.textView_info);
             more = (ImageButton) v.findViewById(R.id.imageButton);
+            filesize = (TextView) v.findViewById(R.id.textView_size);
+
+            this.position = position;
 
 
-            listener = new PopMenuListener(v);
+            listener = new PopMenuListener(v, position);
 
 
             more.setOnClickListener(new View.OnClickListener() {
@@ -154,92 +177,223 @@ public class FileAdapter extends BaseAdapter {
 
         }
 
-        public void bindData(File f) {
+        public void bindData(File f) throws Exception {
 
+            file = f;
             icon.setImageResource(f.isFile()
                     ? R.drawable.ic_attachment_24dp : R.drawable.ic_folder_open_24dp);
             fileName.setText(f.getName());
 
             int size = (f.list() == null) ? 0 : f.list().length;
             fileInfo.setText(f.isFile()
-                    ? String.format("文件：%d 字节", f.length())
-                    : String.format("目录：%d 文件",size));
+                    ? String.format("文件：%d 字节", getFileSize(f))
+                    : String.format("目录：%d 文件", size));
+
+
+            if(map.get(f.getName())==null) {
+                long fileSize;
+                String filesizeStr = "";
+                if (!f.isFile()) {
+
+                    fileSize = getFileSizes(f);
+                    filesizeStr = FormatFileSize(fileSize);
+                    filesize.setText(filesizeStr);
+                    map.put(f.getName(), filesizeStr);
+                }
+            }else {
+                filesize.setText(map.get(f.getName()));
+            }
 
             listener.setFile(f);
 
-
         }
-
-
 
         /**
-         * 菜单监听器
+         * 用于保存数据
          */
-        class PopMenuListener implements PopupMenu.OnMenuItemClickListener {
+        class setData {
 
-            /**
-             * 操作文件
-             */
-            private File file;
-            private View v;
+            private String[] saveData;
 
-            public PopMenuListener(View v){
-                this.v = v;
-            }
+            public setData() {
 
-            /**
-             * 菜单项点击
-             *
-             * @param item  事件源
-             * @return true  事件处理完毕
-             */
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-
-                switch(item.getItemId()){
-                    case R.id.action_copy:
-                        doCopy();
-                        break;
-                    case R.id.action_remove:
-                        doRemove();
-                        break;
-                    case R.id.action_rename:
-                        doRename();
-                        break;
-
-                }
-                return true;
-            }
-
-            private void doRename() {
-                showToast("重命名");
-               // showRenameDialog(v);
-            }
-
-
-
-            private void doRemove() {
-
-                file.delete();
-                showToast("删除");
-                notifyDataSetChanged();
-            }
-
-            private void doCopy() {
-                showToast("复制");
-
-            }
-
-            private void showToast(String msg) {
-                Toast.makeText(context,msg+file.getName(),Toast.LENGTH_SHORT).show();
-            }
-
-            public void setFile(File file) {
-
-                this.file = file;
+                saveData = new String[data.size()];
 
             }
         }
+
+
+//        private void re(){
+//
+//            file.delete();
+////            showToast("删除");
+//            data.remove(position);
+//            Log.v("aa",String.valueOf(position));
+//            notifyDataSetChanged();
+//        }
+
+        private String FormatFileSize(long size) {
+
+            DecimalFormat decimalFormat = new DecimalFormat("#.00");
+            String fileSizeStr = "";
+            String wrongSize = "0B";
+            if (size == 0) {
+                return wrongSize;
+            }
+            if (size < 1024) {
+                fileSizeStr = decimalFormat.format((double) size) + "B";
+            } else if (size < 1048576) {
+                fileSizeStr = decimalFormat.format((double) size / 1024) + "KB";
+            } else if (size < 1073741824) {
+                fileSizeStr = decimalFormat.format((double) size / 1048576) + "MB";
+            } else {
+                fileSizeStr = decimalFormat.format((double) size / 1073741824) + "GB";
+            }
+
+
+            return fileSizeStr;
+        }
+
+
+        private long getFileSize(File file) throws Exception {
+
+            long size = 0;
+            if (file.exists()) {
+
+                FileInputStream fis = null;
+                fis = new FileInputStream(file);
+                size = fis.available();
+                fis.close();
+            }
+
+            return size;
+        }
+
+        private long getFileSizes(File file) throws Exception {
+
+            long size = 0;
+            if (file.exists()) {
+
+                File[] files_open = file.listFiles();
+                for (File f : files_open) {
+                    if (f.isFile()) {
+                        size += getFileSize(f);
+                    } else {
+                        size += getFileSizes(f);
+                    }
+                }
+            }
+
+            return size;
+        }
+
+
     }
+
+
+    /**
+     * 菜单监听器
+     */
+    class PopMenuListener implements PopupMenu.OnMenuItemClickListener {
+
+        /**
+         * 操作文件
+         */
+        private File file;
+        private View v;
+        private int position;
+
+        public PopMenuListener(View v, int position) {
+            this.v = v;
+            this.position = position;
+        }
+
+        /**
+         * 菜单项点击
+         *
+         * @param item 事件源
+         * @return true  事件处理完毕
+         */
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.action_copy:
+                    doCopy();
+                    break;
+                case R.id.action_remove:
+                    doRemove();
+                    break;
+                case R.id.action_rename:
+                    doRename();
+                    break;
+
+            }
+            return true;
+        }
+
+        private void doRename() {
+
+            View view = layoutInflater.inflate(R.layout.rename_demo, null);
+            final EditText editText = (EditText) view.findViewById(R.id.editText);
+            Button btn_ok = (Button) view.findViewById(R.id.btn_ok);
+            Button btn_cancel = (Button) view.findViewById(R.id.btn_cancel);
+            final Dialog dialog = new AlertDialog.Builder(context)
+                    .setTitle("重命名")
+                    .setView(view)
+                    .create();
+            dialog.show();
+
+            btn_ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String newName = editText.getText().toString();
+                    File newFile = new File(file.getParentFile().getAbsolutePath() + "/" + newName);
+                    file.renameTo(newFile);
+                    dialog.dismiss();
+                    data.remove(position);
+                    Log.v("aa", String.valueOf(position));
+                    data.add(newFile);
+                    notifyDataSetChanged();
+                }
+            });
+
+            btn_cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+
+        }
+
+
+        private void doRemove() {
+
+            file.delete();
+            showToast("删除");
+            data.remove(position);
+            Log.v("aa", String.valueOf(position));
+            notifyDataSetChanged();
+
+        }
+
+        private void doCopy() {
+            showToast("该功能暂时还未实现，敬请期待！");
+
+        }
+
+        private void showToast(String msg) {
+            Toast.makeText(context, msg , Toast.LENGTH_SHORT).show();
+        }
+
+        public void setFile(File file) {
+
+            this.file = file;
+
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/xhy/file/MainActivity.java
+++ app/src/main/java/com/example/xhy/file/MainActivity.java
@@ -1,27 +1,57 @@
 package com.example.xhy.file;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.StatFs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.text.format.Formatter;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    long SDkSize;
+    long SDzSize;
+
+
+    // 当前打开的文件
+    private File file;
+    private File[] files;
+
     //  视图
     ListView listView;
+    TextView textView_path;
+    ProgressBar progressBar;
+
+    // 文件的绝对路径
+    private String abpath;
 
     //  数据
     ArrayList<File> data;
@@ -30,18 +60,56 @@ public class MainActivity extends AppCompatActivity {
     FileAdapter adapter;
     Stack<File> stackfiles;
 
+
+    private boolean isfinish = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //  加载布局
         setContentView(R.layout.activity_main);
         listView = (ListView) findViewById(R.id.listView);
+        textView_path = (TextView) findViewById(R.id.textView_path);
 
         initView();
-
+        setProgressPercent();
 
     }
+
+    private void setProgressPercent() {
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        TextView textView_avi = (TextView) findViewById(R.id.textView_avi);
+        TextView textView_tol = (TextView) findViewById(R.id.textView_tol);
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            StatFs fs = new StatFs(Environment.getExternalStorageDirectory().getPath());
+
+            SDzSize = fs.getTotalBytes();
+            int zsize = (int) (SDzSize / 1024 / 1024 / 1024);
+            progressBar.setMax(zsize);
+
+            SDkSize = fs.getAvailableBytes();
+            int ksize = (int) (SDkSize / 1024 / 1024 / 1024);
+            progressBar.setProgress(ksize);
+
+            textView_avi.setText("可用:" + String.valueOf(ksize) + "GB");
+            textView_tol.setText("总共:" + String.valueOf(zsize) + "GB");
+
+        } else {
+            textView_avi.setText("内存异常，无法显示数据");
+        }
+
+    }
+
+//    private void replaceBar(){
+//
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+////        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+////        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_keyboard_backspace_24dp);
+//
+//
+//    }
 
 
     private void initView() {
@@ -51,18 +119,22 @@ public class MainActivity extends AppCompatActivity {
         //2.
         //  加载SD卡中的文件
         //  外部存储文件
-        File sdPath = Environment.getExternalStorageDirectory();
-        loadData(sdPath);
+        file = Environment.getExternalStorageDirectory();
+        loadData(file);
+        makeAdapter();
+    }
+
+    private void makeAdapter() {
         adapter = new FileAdapter(this, data);               //3.
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            File file;
+
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 file = data.get(position);
-                if(!file.isFile()) {
+                if (!file.isFile()) {
                     loadData(file);
                     adapter.notifyDataSetChanged();
                 } else {
@@ -77,16 +149,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
     private void loadData(File file) {
 
-        stackfiles.push(file.getParentFile());
+        abpath = "当前位置：" + file.getAbsolutePath();
+        if (!file.getName().equals("0")) {
+
+            stackfiles.push(file.getParentFile());
+
+        }
+
+        textView_path.setText(abpath);
+
 
         data.clear();
         //  获得目录中的所有文件
-        File[] files = file.listFiles();
+        files = file.listFiles();
 
         for (File f : files) {
             data.add(f);
@@ -98,25 +177,287 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.actionbar_menu,menu);
+        getMenuInflater().inflate(R.menu.actionbar_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.item_sear);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setQueryHint("请输入文件名");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(item.getItemId() == R.id.item_back){
+        switch (item.getItemId()) {
 
-            if(stackfiles!=null) {
-                File file = stackfiles.pop();
+            case R.id.item_back:
+                doBack();
+                break;
+            case R.id.item_add:
+                doAdd();
+                break;
+            case R.id.action_sort_size:
+                Toast.makeText(MainActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                try {
+                    doSort_size();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.action_sort_word:
+                doSort_word();
+                break;
+            case R.id.action_sort_date:
+                showToast("该功能暂时还未实现，敬请期待");
+                break;
+            case R.id.action_sort_type:
+                showToast("该功能暂时还未实现，敬请期待");
+                break;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void doSort_word() {
+
+        // 得到该目录下所有文件
+        File[] files_sort = files;
+        File[] files_sort_lat = new File[files.length];
+        String[] words = new String[files.length];
+
+        for (int i = 0; i < files_sort.length; i++) {
+
+            words[i] = files_sort[i].getName();
+            files_sort_lat[i] = files_sort[i];
+
+            if (i > 0) {
+                for (int j = i; j > 0; j--) {
+                    if (String_Sort(words[j], files_sort_lat[j - 1].getName()) == 1) {
+
+                        String temp = words[j];
+                        words[j] = words[j - 1];
+                        words[j - 1] = temp;
+
+                        File file = files_sort_lat[j];
+                        files_sort_lat[j] = files_sort_lat[j - 1];
+                        files_sort_lat[j - 1] = file;
+
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        data.clear();
+        for (File file : files_sort_lat) {
+            data.add(file);
+        }
+        adapter.notifyDataSetChanged();
+
+    }
+
+    private int String_Sort(String str1, String str2) {
+
+        char[] c1 = str1.toCharArray();
+        char[] c2 = str2.toCharArray();
+
+
+        if ((64 < c1[0] && c1[0] < 91) || (96 < c1[0] && c1[0] < 123)) {
+            if ((64 < c1[0] && c1[0] < 91) || (96 < c1[0] && c1[0] < 123)) {
+                if (c1[0] < c2[0]) {
+                    return 1;
+                } else {
+                    return 2;
+                }
+            } else {
+                return 1;
+            }
+        } else {
+            if ((64 < c1[0] && c1[0] < 91) || (96 < c1[0] && c1[0] < 123)) {
+                return 2;
+            } else {
+                return 1;
+            }
+        }
+
+
+    }
+
+
+    /**
+     * 按大小排序
+     *
+     * @throws Exception
+     */
+    private void doSort_size() throws Exception {
+
+        // 得到该目录下所有文件
+        File[] files_sort = files;
+        File[] files_sort_lat = new File[files.length];
+        long[] longs = new long[files.length];
+        long sizeFile;
+        for (int i = 0; i < files_sort.length; i++) {
+
+            if (files_sort[i].isFile()) {
+                sizeFile = getFileSize(files_sort[i]);
+            } else {
+                sizeFile = getFileSizes(files_sort[i]);
+            }
+            longs[i] = sizeFile;
+
+            files_sort_lat[i] = files_sort[i];
+
+            if (i > 0) {
+                for (int j = i; j > 0; j--) {
+                    if (longs[j] > longs[j - 1]) {
+                        long temp = longs[j];
+                        longs[j] = longs[j - 1];
+                        longs[j - 1] = temp;
+
+                        File file = files_sort_lat[j];
+                        files_sort_lat[j] = files_sort_lat[j - 1];
+                        files_sort_lat[j - 1] = file;
+
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        data.clear();
+        for (File file : files_sort_lat) {
+            data.add(file);
+        }
+        adapter.notifyDataSetChanged();
+
+    }
+
+    private long getFileSize(File file) throws Exception {
+
+        long size = 0;
+        if (file.exists()) {
+
+            FileInputStream fis = null;
+            fis = new FileInputStream(file);
+            size = fis.available();
+            fis.close();
+        }
+
+        return size;
+    }
+
+    private long getFileSizes(File file) throws Exception {
+
+        long size = 0;
+        if (file.exists()) {
+
+            File[] files_open = file.listFiles();
+            for (File f : files_open) {
+                if (f.isFile()) {
+                    size += getFileSize(f);
+                } else {
+                    size += getFileSizes(f);
+                }
+            }
+        }
+
+        return size;
+    }
+
+
+    /**
+     * 添加文件
+     */
+    private void doAdd() {
+
+        View v = getLayoutInflater().inflate(R.layout.rename_demo, null);
+        final EditText editText = (EditText) v.findViewById(R.id.editText);
+        Button btn_ok = (Button) v.findViewById(R.id.btn_ok);
+        Button btn_cancel = (Button) v.findViewById(R.id.btn_cancel);
+        final Dialog dialog = new AlertDialog.Builder(this)
+                .setTitle("新建文件夹")
+                .setView(v)
+                .create();
+        dialog.show();
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                name = file.getAbsolutePath() + "/" + name;
+                Log.v("filename", name);
+                File newFile = new File(name);
+                newFile.mkdir();
                 loadData(file);
                 adapter.notifyDataSetChanged();
-
+                dialog.dismiss();
             }
+        });
 
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+    }
+
+    /**
+     * 返回上一级
+     */
+    public void doBack() {
+
+
+        if (stackfiles.size() > 0) {
+            File filepar = stackfiles.pop();
+            file = filepar;
+            loadData(filepar);
+            adapter.notifyDataSetChanged();
+        } else if (stackfiles.size() == 0) {
+            Toast.makeText(MainActivity.this, "已经是根目录，无法回到上一级", Toast.LENGTH_SHORT).show();
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        if (isfinish) {
+            finish();
+        } else {
+            Toast.makeText(MainActivity.this, "再按一次退出", Toast.LENGTH_SHORT).show();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    isfinish = true;
+                    try {
+                        Thread.sleep(2000);
+                        isfinish = false;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }).start();
+        }
+
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(MainActivity.this, msg , Toast.LENGTH_SHORT).show();
     }
 
     class MyFilter implements FileFilter {
